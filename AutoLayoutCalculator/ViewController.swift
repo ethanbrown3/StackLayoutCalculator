0f136cf9c22cab85c081767ae3a36b5dc4c5a296
Ethan Brown <ethanbrown3@mail.weber.edu>
2017-12-29 13:31:20 -0700

    first commit

---
+++ AutoLayoutCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayoutCalculator
+//
+//  Created by Ethan Brown on 12/29/17.
+//  Copyright © 2017 Ethan Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
